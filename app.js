6d5b3a7db2acaeb5c79cb78ea5a1716fb8226757
billiamwilliam <sheltowt@gmail.com>
2015-03-14 15:23:21 -0700

    adding worker model

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var application_root = __dirname,
   http = require('http'),
   mongoose = require('mongoose'),
   bodyParser = require('body-parser'),
-  autoIncrement = require('mongoose-auto-increment')
+  autoIncrement = require('mongoose-auto-increment'),
   workerFarm = require('worker-farm'),
   twitterWorker = workerFarm(require.resolve('./workers/twitter'));
 
@@ -14,59 +14,58 @@ app.use(bodyParser.json())
 app.use(express.static(path.join(application_root, "public")));
 
 // Database
-
 var connection = mongoose.connect('mongodb://localhost/workers');
 
 autoIncrement.initialize(connection);
 
 var Schema = mongoose.Schema;  
 
-var Worker = new Schema({
+var Job = new Schema({
 		id: Schema.Types.ObjectId, 
     status: { type: String, required: true },  
-    data: { type: Schema.Types.Mixed, required: true },   
+    url: { type: String, required: true },   
     modified: { type: Date, default: Date.now }
 });
 
-Worker.plugin(autoIncrement.plugin, 'WorkerModel')
+Job.plugin(autoIncrement.plugin, 'JobModel')
 
-var WorkerModel = connection.model('Worker', Worker);
+var JobModel = connection.model('Job', Job);
 
 app.get('/api', function (req, res) {
 	console.log('Node Worker Bee API is running');
   res.send('Node Worker Bee API is running');
 });
 
-app.get('/api/workers', function (req, res){
-	console.log('GET /api/workers');
-  return WorkerModel.find(function (err, workers) {
+app.get('/api/jobs', function (req, res){
+	console.log('GET /api/jobs');
+  return JobModel.find(function (err, jobs) {
     if (!err) {
-      return res.send(workers);
+      return res.send(jobs);
     } else {
       return console.log(err);
     }
   });
 });
 
-app.post('/api/workers', function (req, res){
-	console.log('POST /api/workers');
+app.post('/api/jobs', function (req, res){
+	console.log('POST /api/jobs');
   var worker;
   console.log("POST: ");
   console.log(req.body);
-  worker = new WorkerModel({
+  job = new JobModel({
     status: req.body.status,
-    data: req.body.data,
+    url: req.body.url,
     modified: req.body.modified
   });
   for (var i = 0; i <10; i++) {
-  	twitterWorker('#' + i + ' FOO', function (err, outp) {
+  	twitterWorker("http://echo.jsontest.com/key/value/one/two", function (err, outp) {
   		console.log(outp)
   		if (i == 10) {
   			workerFarm.end(twitterWorker)
   		}
   	})
   }
-  worker.save(function (err) {
+  job.save(function (err) {
     if (!err) {
       return console.log("created");
 
@@ -74,41 +73,41 @@ app.post('/api/workers', function (req, res){
       return console.log(err);
     }
   });
-  return res.send(worker);
+  return res.send(job);
 });
 
-app.get('/api/workers/:id', function (req, res){
-	console.log('GET /api/workers/id');
-  return WorkerModel.findById(req.params.id, function (err, worker) {
+app.get('/api/jobs/:id', function (req, res){
+	console.log('GET /api/jobs/id');
+  return JobModel.findById(req.params.id, function (err, job) {
     if (!err) {
-      return res.send(worker);
+      return res.send(job);
     } else {
       return console.log(err);
     }
   });
 });
 
-app.put('/api/workers/:id', function (req, res){
-	console.log('PUT /api/workers/id');
-  return WorkerModel.findById(req.params.id, function (err, worker) {
-    worker.title = req.body.title;
-    worker.description = req.body.description;
-    worker.style = req.body.style;
-    return worker.save(function (err) {
+app.put('/api/jobs/:id', function (req, res){
+	console.log('PUT /api/jobs/id');
+  return JobModel.findById(req.params.id, function (err, job) {
+    job.title = req.body.url;
+    job.description = req.body.description;
+    job.style = req.body.style;
+    return job.save(function (err) {
       if (!err) {
         console.log("updated");
       } else {
         console.log(err);
       }
-      return res.send(worker);
+      return res.send(job);
     });
   });
 });
 
-app.delete('/api/workers/:id', function (req, res){
-	console.log('DELETE /api/workers/id');
-  return WorkerModel.findById(req.params.id, function (err, worker) {
-    return worker.remove(function (err) {
+app.delete('/api/jobs/:id', function (req, res){
+	console.log('DELETE /api/jobs/id');
+  return JobModel.findById(req.params.id, function (err, job) {
+    return job.remove(function (err) {
       if (!err) {
         console.log("removed");
         return res.send('');
--- workers/twitter.js
+++ workers/twitter.js
@@ -1,3 +1,57 @@
-module.exports = function (inp, callback) {
-	callback(null, inp + ' BAR (' + process.pid + ')')
+var mongoose = require('mongoose'),
+	connection = mongoose.connect('mongodb://localhost/workers'),
+	http = require('http'),
+	autoIncrement = require('mongoose-auto-increment');
+
+autoIncrement.initialize(connection);
+
+var Schema = mongoose.Schema; 
+
+var Worker = new Schema({
+		id: Schema.Types.ObjectId,
+		url: { type: String, required: true }, 
+    result: { type: Schema.Types.Mixed, required: false },   
+    modified: { type: Date, default: Date.now }
+});
+
+Worker.plugin(autoIncrement.plugin, 'WorkerModel')
+
+var WorkerModel = connection.model('Worker', Worker);
+
+practice = "http://echo.jsontest.com/key/value/one/two"
+
+module.exports = function (url, callback) {
+	callback(null, url + ' BAR (' + process.pid + ')')
+
+	var options ={
+		host: 'http://echo.jsontest.com',
+		path: '/key/value/one/two'
+	}
+
+	var request = http.request(options, function (res) {
+		var data = '';
+    res.on('data', function (chunk) {
+        data += chunk;
+    });
+    res.on('end', function () {
+      console.log(data);
+      worker = new WorkerModel({
+      	url: url,
+      	result: data
+      });
+      worker.save(function (err) {
+    		if (!err) {
+      		return console.log("created");
+    		} else {
+      		return console.log(err);
+    		}
+			});
+    });
+	});
+
+	request.on('error', function (e) {
+    console.log(e.message);
+	});
+	request.end();
+
 }
